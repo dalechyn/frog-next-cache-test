523463bb6a1532c6aadfb83d2ce00e924096799e
hotwater <vlad.dalechin@gmail.com>
2024-03-29 23:43:47 +0200

    test: see if svg is also cached

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -9,7 +9,7 @@ import { serveStatic } from 'frog/serve-static'
 const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
-  imageOptions: {headers: { 'Cache-Control': 'max-age=0, s-maxage=0',
+  imageOptions: {format: 'svg',headers: { 'Cache-Control': 'max-age=0, s-maxage=0',
       'CDN-Cache-Control': 'max-age=0',
       'Vercel-CDN-Cache-Control': 'max-age=0',}}
   // Supply a Hub to enable frame verification.
